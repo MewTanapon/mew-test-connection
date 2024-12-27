a47be40c31b7027eab7424c09c522e33fcf1abb3
MewTanapon <tanapon.tanavejwasin@gmail.com>
2024-12-27 10:04:28 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+from st_files_connection import FilesConnection
+from google.oauth2 import service_account
+from google.cloud import storage
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# conn = st.connection('gcs', type=FilesConnection)
+# gcs_file_path = 'mew_test_connection_bucket/file/status.csv'
+# print(conn)
+# df = conn.read(gcs_file_path, input_format="csv", ttl=600)
+# service_account_info = st.secrets["gcp_service_account"]
+# credentials = service_account.Credentials.from_service_account_info(service_account_info)
+# client = storage.Client(credentials=credentials)
+# buckets = list(client.list_buckets())
+
+
+# project_id = 'mew-test-connection-project'
+# bucket_name = 'mew_test_connection_bucket'
+# file_path = 'file/status.csv'
+
+# bucket = storage.Client(project=project_id).bucket(bucket_name)
+# df = bucket.blob(file_path)
+
+st.write(st.secrets)
+print(st.secrets)
+# st.write(df)
