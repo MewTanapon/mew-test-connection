fd661b65079815412a70ee880caf394c1efadcaf
MewTanapon <tanapon.tanavejwasin@gmail.com>
2024-12-27 10:24:55 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,22 +4,22 @@ from google.oauth2 import service_account
 from google.cloud import storage
 import pandas as pd
 
-# conn = st.connection('gcs', type=FilesConnection)
-# gcs_file_path = 'mew_test_connection_bucket/file/status.csv'
-# print(conn)
-# df = conn.read(gcs_file_path, input_format="csv", ttl=600)
-service_account_info = st.secrets["gcp_service_account"]
-credentials = service_account.Credentials.from_service_account_info(service_account_info)
-client = storage.Client(credentials=credentials)
-buckets = list(client.list_buckets())
-st.write(buckets)
+conn = st.connection('gcs', type=FilesConnection)
+gcs_file_path = 'mew_test_connection_bucket/file/status.csv'
+st.write('connection success', conn)
+df = conn.read(gcs_file_path, input_format="csv", ttl=600)
+# service_account_info = st.secrets["gcp_service_account"]
+# credentials = service_account.Credentials.from_service_account_info(service_account_info)
+# client = storage.Client(credentials=credentials)
+# buckets = list(client.list_buckets())
+# st.write(buckets)
 
-project_id = 'mew-test-connection-project'
-bucket_name = 'mew_test_connection_bucket'
-file_path = 'file/status.csv'
+# project_id = 'mew-test-connection-project'
+# bucket_name = 'mew_test_connection_bucket'
+# file_path = 'file/status.csv'
 
-bucket = storage.Client(project=project_id).bucket(bucket_name)
-blob = bucket.blob(file_path)
-content = blob.download_as_text()
-df = pd.read_csv(pd.compat.StringIO(content))
+# bucket = storage.Client(project=project_id).bucket(bucket_name)
+# blob = bucket.blob(file_path)
+# content = blob.download_as_text()
+# df = pd.read_csv(pd.compat.StringIO(content))
 st.write(df)
